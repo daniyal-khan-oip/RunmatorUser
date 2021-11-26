c7494c2953285dc33986784e2ca07f2684308ef2
Syed Roshaan Ali <roshaan.ali@binaryit.co>
2021-11-26 14:51:43 +0500

    redux and redux persist integrated

---
+++ src/store/Actions/index.js
@@ -0,0 +1,49 @@
+import * as types from './actionType';
+
+// Auth Actions
+export const user_login = data => async dispatch => {
+  try {
+    console.log(data, '=================');
+  } catch (error) {
+    console.log('Network Error');
+  }
+};
+
+// export const userLoggedIn = (email, password) => async dispatch => {
+//   dispatch({
+//     type: types.USER_LOGIN,
+//     payload: {isApiCall: true},
+//   });
+//   try {
+//     const response = await axios.post(`${baseUrl}/login`, {
+//       email,
+//       password,
+//     });
+//     if (response.data.status) {
+//       dispatch({
+//         type: types.USER_LOGIN,
+//         payload: {isUserLogin: response.data.status, ...response.data.data},
+//       });
+//     } else {
+//       toast.error(response.data.msg);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     toast.error('Network Error');
+//   }
+//   dispatch({
+//     type: types.USER_LOGIN,
+//     payload: {isApiCall: false},
+//   });
+// };
+
+// export const userLogOut = () => async dispatch => {
+//   dispatch({
+//     type: types.USER_LOGOUT,
+//     payload: {isUserLogin: false, isApiCall: false},
+//   });
+//   dispatch({
+//     type: types.GET_METRICS,
+//     payload: [],
+//   });
+// };
